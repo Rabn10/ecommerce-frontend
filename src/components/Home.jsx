b0f208db8adcf83b6c63c35dc9a4b191202e4b22
Rabin <rabin.awale09@gmail.com>
2026-02-01 15:41:29 +0545

    initialize React project with Vite, add basic structure and components

---
+++ src/components/Home.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import LatestProducts from "./common/LatestProducts";
+import FeaturedProducts from "./common/FeaturedProducts";
+import Hero from "./common/Hero";
+import Layout from "./common/Layout";
+
+// import SliderTwoImg from '../../assets/images/banner-2.jpg';
+
+
+
+const Home = () => {
+    return (
+        <>
+
+            <Layout>
+                <Hero />
+                <LatestProducts />
+                <FeaturedProducts />
+            </Layout>
+
+
+
+
+
+
+        </>
+    )
+}
+
+export default Home;
