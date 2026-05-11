e2aad5fc75fdc4c33c9fa9600cba098025e0f0c4
Rabin <rabin.awale09@gmail.com>
2026-05-11 17:26:43 +0545

    features: implement order confirmation page and loading state

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,6 +4,7 @@ import { Link, useNavigate } from 'react-router-dom'
 import { useForm } from 'react-hook-form';
 import { apiUrl } from './common/http';
 import { AuthContext } from './context/Auth';
+import { toast } from 'react-toastify';
 
 const Login = () => {
 
